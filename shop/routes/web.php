f2e0c13c9d89c9ad1175629d06abf3f65ff1868b
julia <julia.vinals@inslpineda.cat>
2025-02-04 10:30:07 +0100

    update

--- shop/routes/web.php
+++ shop/routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+//incluir controlador
+use App\Http\Controllers\CategoriaController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resources([
+    'categoria'=> CategoriaController::class
+]);
+
+
