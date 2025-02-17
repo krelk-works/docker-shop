49efc6ac0b3443e32cfe99f510821007bea3f40f
Leo <leo.craciun97@gmail.com>
2025-02-17 12:01:25 +0100

    update

--- shop/resources/views/home.blade.php
+++ shop/resources/views/home.blade.php
@@ -1,34 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
+@extends('layouts.app')
 
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-    <title>Document</title>
-</head>
-<body>
-    <h1>Benvingut :D</h1>
-    <table>
-        <tr>
-            <td><a href="{{route('altaCategoria')}}"><button>Alta Categoria</button></a></td>
-            <td><a href="{{route('altaCalzado')}}"><button>Alta Calzado</button></a></td>
-        </tr>
-        <tr>
-            <button type="button" class="btn btn-primary">Primary</button>
-            <button type="button" class="btn btn-secondary">Secondary</button>
-            <button type="button" class="btn btn-success">Success</button>
-            <button type="button" class="btn btn-danger">Danger</button>
-            <button type="button" class="btn btn-warning">Warning</button>
-            <button type="button" class="btn btn-info">Info</button>
-            <button type="button" class="btn btn-light">Light</button>
-            <button type="button" class="btn btn-dark">Dark</button>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-            <button type="button" class="btn btn-link">Link</button>
-        </tr>
-    </table>
-</body>
-</html>
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- shop/routes/web.php
+++ shop/routes/web.php
@@ -23,3 +23,7 @@ Route::resources([
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
